f7a8416fd1cad9ad7fded4b92e1d1731193cb6c9
Giulio Smedile <giu.smedile1@stud.uniroma3.it>
2022-06-27 20:02:53 +0200

    Initial

---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_iBJ56HJ3I', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '3peuo13ghjq51jnenhh9svi47u' //is this used anywhere?
+    },
+};
+
