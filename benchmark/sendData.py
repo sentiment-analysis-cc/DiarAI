516eaf59f4f6a28d2a22339f54a74d5df1330b6e
Emanuele Volanti <emavolant@gmail.com>
2022-07-17 19:10:27 +0200

    Add incremental stop mode

--- benchmark/sendData.py
+++ benchmark/sendData.py
@@ -1,4 +1,4 @@
-from tracemalloc import start
+from tracemalloc import start, stop
 import requests
 from essential_generators import DocumentGenerator
 import threading
@@ -7,6 +7,7 @@ import string
 import time
 from autoLogin import *
 import argparse
+from tqdm import tqdm
 
 basepathWrite = "https://blwdljp75pvc5eswhthjx66a4m0hdbyv.lambda-url.us-east-1.on.aws/"
 basepathRead = "https://uuq3nqiwkutez37nremubegf6i0xqjsz.lambda-url.us-east-1.on.aws/"
@@ -22,13 +23,13 @@ parser.add_argument('--threads', '-t', type=int,
 parser.add_argument('--text-len', '--num-req', type=int, 
                     help='Length of text array, or number of request per thread', default=sizeText)
 
-parser.add_argument('--stop-mode' , '-sm', type=str, choices=['time', 'num'],
+parser.add_argument('--stop-mode' , '-sm', type=str, choices=['time', 'num', 'incremental'],
                     help='The stopping mode', default='time')
 
 parser.add_argument('--execution-time' , '-et', type=int,
                     help='Execution time on a single thread', default=deltaTime)
 
-parser.add_argument('--create-users', '-c',action='store_true',
+parser.add_argument('--create-users', '-c', action='store_true',
                     help='Sign up new users')
 
 parser.add_argument('--mode', '-m', type=str, required=True, choices=['write', 'read'],
@@ -38,15 +39,15 @@ args = parser.parse_args()
 
 numThreads, sizeText, stopMode, deltaTime, reSignUp, mode = vars(args).values()
 
+lst = [10, 20, 50, 100, 100, 100, 70, 50, 20]
+stepLen = deltaTime / len(lst)
+
 ### Functions ###
 
-def initTexts(num):
-    lst = []
+def initTexts(num, res):
     main = DocumentGenerator()
-    
-    for _ in range(num):
-        lst.append(main.sentence() + " " + main.sentence())
-    return lst
+    for _ in tqdm(range(num)):
+        res.append(main.sentence() + " " + main.sentence())
 
 def createUsers(num):
     lst = []
@@ -62,10 +63,9 @@ def loginUsers(lst):
 def numberedLogin(num):
     return loginUsers(list(map(lambda x: f"test{x}", range(num))))
 
-def doWriteRequest(result, token, i):
-    texts = initTexts(sizeText)
+def doWriteRequest(result, token, i, textList):
+    texts = textList
     index = 0
-    print("[DEBUG] Text list created! Thread number " + str(i))
     start_time = time.time()
     localEndTime = start_time + deltaTime
     # Print current time in humanly readable format
@@ -73,16 +73,25 @@ def doWriteRequest(result, token, i):
         s= time.strftime("%H:%M:%S", time.localtime(start_time))
         e= time.strftime("%H:%M:%S", time.localtime(localEndTime))
         print(f"[DEBUG] Start time for thread {i}: {s} - End time: {e}")
-    while time.time() <= localEndTime or stopMode == 'num':
+    while time.time() <= localEndTime or stopMode == 'num' or stopMode == "incremental":
         params = ({"diaryTitle": getRandomString(20), "text": texts[index], "token": token})
-        res = requests.get(basepathWrite, params=params)
-        print("[DEBUG] Status Code: " + str(res.status_code) + "; Thread number " + str(i))
+        active = True
+        if stopMode == "incremental":
+            active = canIRun(i)
+            print(f"Thread: {i}; canIRun: {active}")
+        if active:
+            res = requests.get(basepathWrite, params=params)
+            print("[DEBUG] Status Code: " + str(res.status_code) + "; Thread number " + str(i))
+        else:
+            time.sleep(stepLen)
         index = (index + 1) % len(texts)
         if index == 0:
-            if stopMode == "time":
+            if stopMode == "time" or stopMode == "incremental":
                 random.shuffle(texts)
             elif stopMode == "num":
                 break
+        if stopMode == "incremental" and time.time() > globalTime + deltaTime:
+            break
     print("--- Thread number " + str(i) + " finished! ---")
     result.append(time.time() - start_time)
 
@@ -94,12 +103,20 @@ def doReadRequest(result, token, i):
         s= time.strftime("%H:%M:%S", time.localtime(start_time))
         e= time.strftime("%H:%M:%S", time.localtime(localEndTime))
         print(f"[DEBUG] Start time for thread {i}: {s} - End time: {e}")
-    while time.time() <= localEndTime or stopMode == 'num':
+    while time.time() <= localEndTime or stopMode == 'num' or stopMode == "incremental":
         params = ({"type": "all", "token": token})
-        res = requests.get(basepathRead, params=params)
-        print("[DEBUG] Status Code: " + str(res.status_code) + "; Thread number " + str(i))
+        active = True
+        if stopMode == "incremental":
+            active = canIRun(i)
+        if active:
+            res = requests.get(basepathRead, params=params)
+            print("[DEBUG] Status Code: " + str(res.status_code) + "; Thread number " + str(i))
+        else:
+            time.sleep(stepLen)
         index += 1
         if stopMode == "num" and index >= sizeText:
+            break
+        elif stopMode == "incremental" and time.time() > globalTime + deltaTime:
             break
     print("--- Thread number " + str(i) + " finished! ---")
     result.append(time.time() - start_time)
@@ -108,14 +125,22 @@ def doReadRequest(result, token, i):
 def getRandomString(num):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k = num))
 
+def canIRun(index):
+    currentTime = time.time() - globalTime
+    ind = int(currentTime // stepLen)
+    return index < numThreads * (lst[(ind - 1) % len(lst)] / 100)
+
 
 print(f'Current mode: {mode}')
 print(f'Number of thread: {numThreads}')
 if stopMode == 'time':
     print(f'Text list size: {sizeText}')
     print(f'Execution time: {deltaTime}s')
-else:
+elif stopMode == 'num':
     print(f'Number of request per thread: {sizeText}')
+elif stopMode == 'incremental':
+    print(f'Text list size: {sizeText}')
+    print(f'Incremental time: {deltaTime}s')
 print()
 
 
@@ -128,13 +153,31 @@ else:
     print("--- Logging in users ---")
     tokens = numberedLogin(numThreads)
 
+
+threads = []
+textMatrix = [[] for _ in range(numThreads)]
+
+if mode == 'write':
+    print("--- Starting to fill texts ---")
+    for i in range(numThreads):
+        t = threading.Thread(target=initTexts, args=[numThreads, textMatrix[i]])
+        t.daemon=True
+        threads.append(t)
+
+    for t in threads: 
+        t.start()
+
+    for t in threads: 
+        t.join()
+
+    print("[DEBUG] Text list created!")
+
 threads = []
 res = []
-
-print("--- Starting to fill texts ---")
+globalTime = time.time()
 for i in range(numThreads):
     if mode == "write":
-        t = threading.Thread(target=doWriteRequest, args=[res, tokens[i], i])
+        t = threading.Thread(target=doWriteRequest, args=[res, tokens[i], i, textMatrix[i]])
     elif mode == "read":
         t = threading.Thread(target=doReadRequest, args=[res, tokens[i], i])
     t.daemon=True
